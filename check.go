40652600f8ef56fb7172880bbcf2ed33d054190a
A1esandr <a1exswan001@gmail.com>
2021-09-06 00:02:09 +0500

    Add construct

--- check.go
+++ check.go
@@ -8,3 +8,11 @@ type (
 		Check(url string) (map[string]string, error)
 	}
 )
+
+func New() Checker {
+	return &checker{}
+}
+
+func (c *checker) Check(url string) (map[string]string, error) {
+	return nil, nil
+}
