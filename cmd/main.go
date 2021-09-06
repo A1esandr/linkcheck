d5a8cdfbe760d1c857d1a2cc8b18523a4011348d
A1esandr <a1exswan001@gmail.com>
2021-09-07 00:06:52 +0500

    Added call

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"os"
 
+	"github.com/A1esandr/linkcheck"
+)
+
+var urlFlag = flag.String("url", "", "URL of the site, for example, https://golang.org")
+
+func main() {
+	flag.Parse()
+	url := os.Getenv("URL")
+	if len(url) == 0 {
+		url = *urlFlag
+	}
+	linkcheck.New().Check(url)
 }
