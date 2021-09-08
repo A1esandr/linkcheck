5c3a9243bc652f354ff194fcc92039437b44c669
A1esandr <a1exswan001@gmail.com>
2021-09-08 12:05:22 +0500

    Added check

--- check.go
+++ check.go
@@ -1,5 +1,13 @@
 package linkcheck
 
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
 type (
 	checker struct {
 	}
@@ -15,4 +23,23 @@ func New() Checker {
 
 func (c *checker) Check(url string) (map[string]string, error) {
 	return nil, nil
+}
+
+func (c *checker) check(url string, count int) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	if resp == nil {
+		return fmt.Errorf("nil response from %s", url)
+	}
+	if resp.StatusCode != http.StatusOK && count < 3 {
+		if count == 2 {
+			return fmt.Errorf("not downloaded %s", url)
+		}
+		log.Println("Error loading", url)
+		time.Sleep(time.Duration(300+rand.Intn(1000)) * time.Millisecond)
+		return c.check(url, count+1)
+	}
+	return nil
 }
